9440346ff86ea317f5866a61d7e71f9896ee62a5
Suhas Dara <suhasdara01@gmail.com>
2021-08-31 23:19:06 +0530

    markdown code block language and copy

--- src/components/markdown/markdownCodeBlock.js
+++ src/components/markdown/markdownCodeBlock.js
@@ -1,7 +1,12 @@
 // Code taken and modified from https://mdxjs.com/guides/syntax-highlighting
 
 import React from "react";
+import { Button } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCopy } from "@fortawesome/free-solid-svg-icons";
 import Highlight, { defaultProps } from "prism-react-renderer";
+
+import "../../styles/components/markdown/markdownCodeBlock.css";
 
 export default function MarkdownCodeBlock({children, className}) {
   const language = className.replace(/language-/, '');
@@ -9,20 +14,32 @@ export default function MarkdownCodeBlock({children, className}) {
   return (
     <Highlight {...defaultProps} code={children} language={language}>
       {({className, style, tokens, getLineProps, getTokenProps}) => (
-        <pre className={className} style={{...style}}>
-          {tokens.map((line, i) => {
-            return i !== tokens.length - 1 ?
-              (
-                <div key={i} {...getLineProps({line, key: i})}>
-                  {line.map((token, key) => (
-                    <span key={key} {...getTokenProps({token, key})} />
-                  ))}
-                </div>
-              ) : (
-                <></>
-              )
-          })}
-        </pre>
+        <>
+          <div className="code-language-bar d-inline-block mb-0 mr-1 px-1">
+            <span>{language}</span>
+          </div>
+          <pre className={className + " d-flex flex-column"} style={{...style}}>
+            <Button
+              variant="dark"
+              className="copy-button ml-auto py-0 px-1"
+              onClick={() => {navigator.clipboard.writeText(children)}}
+            >
+              Copy <FontAwesomeIcon icon={faCopy} />
+            </Button>
+            {tokens.map((line, i) => {
+              return i !== tokens.length - 1 ?
+                (
+                  <div key={i} {...getLineProps({line, key: i})}>
+                    {line.map((token, key) => (
+                      <span key={key} {...getTokenProps({token, key})} />
+                    ))}
+                  </div>
+                ) : (
+                  <></>
+                )
+            })}
+          </pre>
+        </>
       )}
     </Highlight>
   )
