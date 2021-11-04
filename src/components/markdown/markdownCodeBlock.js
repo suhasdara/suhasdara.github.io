4c37ad9a65cc7fde4981410faa80cc14eebe55a5
Suhas Dara <suhasdara01@gmail.com>
2021-11-05 00:40:39 +0530

    code block changes

--- src/components/markdown/markdownCodeBlock.js
+++ src/components/markdown/markdownCodeBlock.js
@@ -1,7 +1,7 @@
 // Code taken and modified from https://mdxjs.com/guides/syntax-highlighting
 
 import React from "react";
-import { Button } from "react-bootstrap";
+import {Button, OverlayTrigger, Tooltip} from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCopy } from "@fortawesome/free-solid-svg-icons";
 import Highlight, { defaultProps } from "prism-react-renderer";
@@ -10,6 +10,7 @@ import "../../styles/components/markdown/markdownCodeBlock.css";
 
 export default function MarkdownCodeBlock({children, className}) {
   const language = className.replace(/language-/, '');
+  const noComments = children.replace(/\s+#.+/g, '');
 
   return (
     <Highlight {...defaultProps} code={children} language={language}>
@@ -19,26 +20,33 @@ export default function MarkdownCodeBlock({children, className}) {
             <div className="code-language-bar d-inline-block mb-0 mr-1 px-1">
               <span>{language}</span>
             </div>
-            <Button
-              variant="dark"
-              className="copy-button d-inline-block ml-auto mb-1 py-0 px-1"
-              onClick={() => {navigator.clipboard.writeText(children)}}
+            <OverlayTrigger
+              delay={{show:0, hide:200}}
+              placement="bottom"
+              overlay={(props) => (
+                <Tooltip id="copy-button-tooltip" {...props}>
+                  <small>Comments are excluded</small>
+                </Tooltip>
+              )}
             >
-              Copy <FontAwesomeIcon icon={faCopy} />
-            </Button>
+              <Button
+                variant="dark"
+                className="copy-button d-inline-block ml-auto mb-1 py-0 px-1"
+                onClick={() => {navigator.clipboard.writeText(noComments)}}
+              >
+                Copy <FontAwesomeIcon icon={faCopy} />
+              </Button>
+            </OverlayTrigger>
           </div>
           <pre className={className + " d-flex flex-column"} style={{...style}}>
             {tokens.map((line, i) => {
-              return i !== tokens.length - 1 ?
-                (
-                  <div key={i} {...getLineProps({line, key: i})}>
-                    {line.map((token, key) => (
-                      <span key={key} {...getTokenProps({token, key})} />
-                    ))}
-                  </div>
-                ) : (
-                  <></>
-                )
+              return i !== tokens.length - 1 && (
+                <div key={i} {...getLineProps({line, key: i})}>
+                  {line.map((token, key) => (
+                    <span key={key} {...getTokenProps({token, key})} />
+                  ))}
+                </div>
+              )
             })}
           </pre>
         </>
